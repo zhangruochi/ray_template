4ece7ae8b1d47c2fd3db0603e424857d5724da38
Richard Zhang <richard.sun@synerontech.com>
2023-02-24 01:51:45 +0800

    fix relative path

--- inference.py
+++ inference.py
@@ -4,14 +4,14 @@ from predictor import Predictor
 from omegaconf import OmegaConf
 from starlette.requests import Request
 from typing import Dict
+import os
 
 app = FastAPI()
 
-cfg = OmegaConf.load("./config.yaml")
-
-@serve.deployment(route_prefix="{}".format(cfg.route))
+@serve.deployment(route_prefix="/inference")
 class MyModel:
     def __init__(self) -> None:
+        cfg = OmegaConf.load(os.path.join(os.path.dirname(__file__), "config.yaml"))
         self.predictor = Predictor(cfg)
     
     async def __call__(self, request: Request) -> Dict:
